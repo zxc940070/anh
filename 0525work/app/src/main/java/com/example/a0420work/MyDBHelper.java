dafc151e7252ae1aa94237bed3061923b48da935
zxc940070 <zxc940070@gmail.com>
2022-05-28 22:35:14 +0800

    0525work

---
+++ 0525work/app/src/main/java/com/example/a0420work/MyDBHelper.java
@@ -0,0 +1,33 @@
+package com.example.a0420work;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+
+
+
+import androidx.annotation.Nullable;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class SQLiteDataBaseHelper extends SQLiteOpenHelper {
+    public SQLiteDataBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
